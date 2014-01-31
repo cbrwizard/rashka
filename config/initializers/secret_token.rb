803dc324ea570eab71136ad6a526d617bcb76455
cbrwizard <cbrwizard@gmail.com>
2014-01-31 13:43:16 +0400

    добавлены стартовые гемы

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rashka::Application.config.secret_key_base = 'd30b2c20208e28afbfff92e25faff5b153b431a80cd8429910c6ab9d23335af7b5b6ad35ee6c1a32096c0b93b0eabf5bca6b21718d51de6163637132872bfccc'
