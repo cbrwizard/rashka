a86ada6e41131d211c8d3634add7be77aa219862
cbrwizard <cbrwizard@gmail.com>
2014-02-01 13:20:48 +0400

    валидация новостей и причин, а также отображение ошибок в админке

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,9 @@
 class News < ActiveRecord::Base
   default_scope order(:id)
+
+  validates_presence_of :title, :text, :link, message: "нужно чем-то заполнить"
+
+  validates_uniqueness_of :text, :title, :link, :message => "не должно повторяться в другой новости"
+
+  validates_length_of :text, :title, :link, :minimum => 2, :maximum => 241, :message => "должен быть длиной от 2 до 240 символов"
 end
--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,3 +1,9 @@
 class Reason < ActiveRecord::Base
   default_scope order(:id)
+
+  validates_presence_of :text, message: "нужно чем-то заполнить"
+
+  validates_uniqueness_of :text, :message => "такая причина уже есть"
+
+  validates_length_of :text, :minimum => 2, :maximum => 241, :message => "должен быть длиной от 2 до 240 символов"
 end
