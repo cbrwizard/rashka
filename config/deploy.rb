b110989c6813f3e0309a483244705d25863edc06
cbrwizard <cbrwizard@gmail.com>
2014-02-15 18:17:40 +0400

    обновленный деплой и проверка ассетов

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,7 @@ namespace :deploy do
     dirs += shared_children.map { |d| File.join(shared_path, d) }
     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
     run "git clone #{repository} #{current_path}"
+    run_locally "scp ~/rashka/config/database.yml ubuntu@54.229.179.125:#{shared_path}"
   end
 
   task :cold do
@@ -56,6 +57,8 @@ namespace :deploy do
     transaction do
       update_code
     end
+
+    run "cd #{current_path}; bundle exec rake assets:precompile RAILS_ENV=production"
   end
 
   desc "Update the deployed code."
@@ -99,39 +102,6 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
-  #desc "Zero-downtime restart of Unicorn"
-  #task :restart, :except => { :no_release => true } do
-  #  run "kill -s USR2 `cat /tmp/unicorn.rashka.pid`"
-  #end
-  #
-  #desc "Start unicorn"
-  #task :start, :except => { :no_release => true } do
-  #  run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  #end
-  #
-  #desc "Stop unicorn"
-  #task :stop, :except => { :no_release => true } do
-  #  run "kill -s QUIT `cat /tmp/unicorn.rashka.pid`"
-  #end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previous version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version."
-    task :default do
-      rollback.repo
-      rollback.cleanup
-    end
-  end
 end
 
 def run_rake(cmd)
