b45b538f02ffe274723bde790bfdbb3359ee665b
cbrwizard <cbrwizard@gmail.com>
2014-02-16 23:42:32 +0400

    админские ассеты прекомпилируется

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rashka::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['admin.js', 'admin.css']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
