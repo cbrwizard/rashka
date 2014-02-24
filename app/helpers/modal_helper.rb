5782bb343483888bae7325b81117cff35ab8f027
cbrwizard <cbrwizard@gmail.com>
2014-02-24 13:30:22 +0400

    модульные окна теперь плавно выезжают

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -12,7 +12,7 @@ module ModalHelper
 
     content = capture(&block)
 
-    content_tag(:div, :id => id, :class => "modal", "aria-hidden" => "true", "aria-labelledby" => "myModalLabel", role: "dialog", tabindex: "-1") do
+    content_tag(:div, :id => id, :class => "modal fade", "aria-hidden" => "true", role: "dialog", tabindex: "-1") do
       content_tag(:div, :class => "modal-dialog") do
         content_tag(:div, :class => "modal-content") do
           content_tag(:div, content, :class => 'modal-body')
