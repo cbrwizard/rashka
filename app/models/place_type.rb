df5bd590da2e419539ecd3b8bd18f2bfb4812cbe
cbrwizard <cbrwizard@gmail.com>
2014-03-14 14:34:27 +0400

    кэш обновляется также после изменения типов мест

--- app/models/place_type.rb
+++ app/models/place_type.rb
@@ -1,5 +1,10 @@
 # Типы мест
 # @example #<PlaceType id: 1, name: "Аэропорт", created_at: "2014-02-15 11:03:30", updated_at: "2014-02-15 11:03:30">
 class PlaceType < ActiveRecord::Base
+  include PlacesCache
   has_many :places, class_name: "Place", foreign_key: "type_id"
+
+  # После изменения базы типов мест обновляет кэш
+  after_create :update_places_cache
+  after_update :update_places_cache
 end
